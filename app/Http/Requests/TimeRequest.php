38ba0539c29e8e660ec86e54604aa0070c8ec118
Oluwakayode Adegbuyi <kayode_adegbuyi@yahoo.com>
2023-09-10 00:30:31 +0100

    Create TimeRequest.php

---
+++ app/Http/Requests/TimeRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TimeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'slack_name' => ['required', 'string'],
+            'track' => ['required', 'string'],
+        ];
+    }
+}
