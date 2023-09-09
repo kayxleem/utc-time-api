f7760d447c1ab9d3023a2deeac544f5b04fdc541
Oluwakayode Adegbuyi <kayode_adegbuyi@yahoo.com>
2023-09-09 22:57:41 +0100

    Create TimeController.php

---
+++ app/Http/Controllers/API/TimeController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Time;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Controllers\Controller;
+
+class TimeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        $slack_name=$request->slack_name;
+        $track=$request->track;
+
+        $time=date("Y-m-d")."T".date("H:i:s")."Z";
+        $current_day = now()->format('l');
+        $data = [
+            'slack_name' => $slack_name,
+            'current_day' => $current_day,
+            'utc_time' => $time,
+            'track' => $track,
+            'github_file_url' => 'app\Http\Controllers\API\TimeController.php',
+            'github_repo_url' => 'https://github.com/kayxleem/utc-time-api',
+            'status_code' => 200,
+        ];
+        $response = response()->json($data);
+
+
+
+        $utc_str = gmdate("M d Y H:i:s", time());
+        $utc = strtotime($utc_str);
+        dd($response);
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Time $time)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Time $time)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Time $time)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Time $time)
+    {
+        //
+    }
+}
