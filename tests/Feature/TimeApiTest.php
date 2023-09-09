44c263d17d2293194521b9eff45243fffd6f4cea
Oluwakayode Adegbuyi <kayode_adegbuyi@yahoo.com>
2023-09-09 22:59:04 +0100

    Create TimeApiTest.php

---
+++ tests/Feature/TimeApiTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TimeApiTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_time_api_returns_result(): void
+    {
+        $response = $this->get('api/');
+
+        $response->assertStatus(200);
+    }
+}
